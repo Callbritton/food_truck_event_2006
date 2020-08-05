c96c80c9ce5912dbddaafd144420a7236d017fda
Chris Allbritton <cla.inbox@gmail.com>
2020-08-05 11:03:29 -0400

    Update test_it_has_attributes to pass

--- test/item_test.rb
+++ test/item_test.rb
@@ -12,7 +12,8 @@ class ClassTest < Minitest::Test
   def test_it_has_attributes
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    assert_equal "Apple Pie (Slice)", item2.name 
+    assert_equal "Apple Pie (Slice)", item2.name
+    assert_equal 2.50, item2.price
   end
 
 end
