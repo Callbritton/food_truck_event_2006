9897a4502566be9da3b511cd4a46e90abf9681fa
Chris Allbritton <cla.inbox@gmail.com>
2020-08-05 11:41:40 -0400

    Add food trucks that sell to event and pass associated test

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,10 @@ class Event
     @food_trucks.map(&:name)
   end
 
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |food_truck|
+      food_truck.check_stock(item) > 0
+   end
+  end
+
 end
