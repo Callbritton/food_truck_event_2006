2315e26ca4bd012d70b2c2262c7dfc67ec15d373
Chris Allbritton <cla.inbox@gmail.com>
2020-08-05 11:19:46 -0400

    Add test_it_can_add_multiple_items

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -32,8 +32,23 @@ class FoodTruckTest < Minitest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     food_truck.stock(item1, 30)
-
     assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_add_multiple_items_to_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+
+    expected = {item1 => 55, item2 => 12}
+
+    assert_equal expected, food_truck.inventory
   end
 
 end
